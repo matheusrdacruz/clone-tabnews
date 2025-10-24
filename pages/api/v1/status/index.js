ab4f0f26bdd9484021940ab3d1f112de75938460
matheuscruz <matheuscruz@touchtec.com.br>
2025-10-24 20:20:25 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,48 @@
 import database from "infra/database.js"
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 AS result');
-  console.log(result.rows)
-  res.status(200).json({ status: "ok" });
+  const databaseName = process.env.POSTGRES_DB;
+
+  const updateAt = new Date().toISOString();
+  const databaseVersionValue = await findDatabaseVersion();
+  const databaseMaxConnectionsValue = await findMaxConnectionsResult()
+  const databaseOpenedConnectionsValue = await findOpenedConnections(databaseName);
+
+  const statusResponse = buildStatusResponse(updateAt, databaseVersionValue, databaseMaxConnectionsValue, databaseOpenedConnectionsValue);
+  console.log(statusResponse);
+  res.status(200).json(statusResponse);
+}
+
+async function findMaxConnectionsResult() {
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections");
+  return databaseMaxConnectionsResult.rows[0].max_connections;
+}
+
+async function findDatabaseVersion() {
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  return databaseVersionResult.rows[0].server_version;
+}
+
+
+async function findOpenedConnections(databaseName) {
+  // pode ser filtrada por usuario, database, etc
+  const query = { text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [databaseName] };
+  const databaseOpenedConnectionsResult = await database.query(query);
+  return databaseOpenedConnectionsResult.rows[0].count;
+}
+
+function buildStatusResponse(updateAt, databaseVersionValue, databaseMaxConnectionsValue, databaseOpenedConnectionsValue) {
+  return {
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  };
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+ const responseBody = await response.json();
+
+ console.log(responseBody);
+
+  const parseUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUpdateAt);
+
+  const databasStatus = responseBody.dependencies.database;
+
+  expect(databasStatus.version).toBeDefined();
+  expect(databasStatus.version).toBe("16.0");
+
+  expect(databasStatus.opened_connections).toBeDefined();
+  expect(databasStatus.opened_connections).toBe(1);
 })
